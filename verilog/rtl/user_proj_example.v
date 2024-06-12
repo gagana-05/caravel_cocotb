fb3cebba9a68b0bf83d74a6db6a5a2e99e557c2b
gagana_05 <gaganas315@gmail.com>
2024-06-12 21:23:38 +0530

    added i2c files

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -36,7 +36,7 @@
  */
 
 module user_proj_example #(
-    parameter BITS = 16
+    parameter BITS = 8
 )(
 `ifdef USE_POWER_PINS
     inout vccd1,	// User area 1 1.8V supply
@@ -50,107 +50,333 @@ module user_proj_example #(
     input wbs_cyc_i,
     input wbs_we_i,
     input [3:0] wbs_sel_i,
-    input [31:0] wbs_dat_i,
+    input [7:0] wbs_dat_i,
     input [31:0] wbs_adr_i,
     output wbs_ack_o,
-    output [31:0] wbs_dat_o,
+    output [7:0] wbs_dat_o,
 
     // Logic Analyzer Signals
-    input  [127:0] la_data_in,
-    output [127:0] la_data_out,
-    input  [127:0] la_oenb,
+    input  [2:0] la_data_in,
+    input  [2:0] la_oenb,
 
     // IOs
-    input  [BITS-1:0] io_in,
-    output [BITS-1:0] io_out,
-    output [BITS-1:0] io_oeb,
+    input  [1:0] io_in,
+    output [1:0] io_out,
+    output [1:0] io_oeb,
 
     // IRQ
-    output [2:0] irq
+    output irq
 );
     wire clk;
     wire rst;
+    wire asy_rst;
 
-    wire [BITS-1:0] rdata; 
-    wire [BITS-1:0] wdata;
-    wire [BITS-1:0] count;
+    // Assuming LA probes [65:64] are for controlling the clk & reset  
+    assign clk = (~la_oenb[0]) ? la_data_in[1]: wb_clk_i;
+    assign rst = (~la_oenb[1]) ? la_data_in[1]: wb_rst_i;
+    assign asy_rst = (~la_oenb[2]) ? la_data_in[2]: wb_rst_i;
 
-    wire valid;
-    wire [3:0] wstrb;
-    wire [BITS-1:0] la_write;
 
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = {{(32-BITS){1'b0}}, rdata};
-    assign wdata = wbs_dat_i[BITS-1:0];
 
-    // IO
-    assign io_out = count;
-    assign io_oeb = {(BITS){rst}};
+    // instantiate i2c
 
-    // IRQ
-    assign irq = 3'b000;	// Unused
+    wire scl_pad_i;
+    wire scl_pad_o;
+    wire scl_padoen_o;
+    wire sda_pad_i;
+    wire sda_pad_o;
+    wire sda_padoen_o;
 
-    // LA
-    assign la_data_out = {{(128-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:64-BITS] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
+    assign scl_pad_i = io_in[0];
+    assign sda_pad_i = io_in[1];
 
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i[BITS-1:0]),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:64-BITS]),
-        .count(count)
+    assign io_out[0] = scl_pad_o;
+    assign io_oeb[0] = scl_padoen_o;
+
+    assign io_out[1] = sda_pad_o;
+    assign io_oeb[1] = sda_padoen_o;
+
+    i2c_master_top i2c_master_top_inst (
+        .wb_clk_i(clk),
+        .wb_rst_i(rst),
+        .arst_i(asy_rst),
+        .wb_adr_i(wbs_adr_i[2:0]),
+        .wb_dat_i(wbs_dat_i),
+        .wb_dat_o(wbs_dat_o),
+        .wb_we_i(wbs_we_i),
+        .wb_stb_i(wbs_stb_i),
+        .wb_cyc_i(wbs_cyc_i),
+        .wb_ack_o(wbs_ack_o),
+        .wb_inta_o(irq),
+        .scl_pad_i(scl_pad_i),
+        .scl_pad_o(scl_pad_o),
+        .scl_padoen_o(scl_padoen_o),
+        .sda_pad_i(sda_pad_i),
+        .sda_pad_o(sda_pad_o),
+        .sda_padoen_o(sda_padoen_o)
     );
 
 endmodule
 
-module counter #(
-    parameter BITS = 16
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output reg ready,
-    output reg [BITS-1:0] rdata,
-    output reg [BITS-1:0] count
-);
 
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 1'b0;
-            ready <= 1'b0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1'b1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
-    end
+`include "i2c_master_defines.v"
+`include "i2c_master_byte_ctrl.v"
+`include "i2c_master_bit_ctrl.v"
+
+module i2c_master_top
+  (
+	wb_clk_i, wb_rst_i, arst_i, wb_adr_i, wb_dat_i, wb_dat_o,
+	wb_we_i, wb_stb_i, wb_cyc_i, wb_ack_o, wb_inta_o,
+	scl_pad_i, scl_pad_o, scl_padoen_o, sda_pad_i, sda_pad_o, sda_padoen_o );
+
+	// parameters
+    parameter ARST_LVL = 1'b1; // asynchronous reset level
+    parameter [6:0] DEFAULT_SLAVE_ADDR  = 7'b111_1110;
+	//
+	// inputs & outputs
+	//
+
+	// wishbone signals
+	input        wb_clk_i;     // master clock input
+	input        wb_rst_i;     // synchronous active high reset
+	input        arst_i;       // asynchronous reset
+	input  [2:0] wb_adr_i;     // lower address bits
+	input  [7:0] wb_dat_i;     // databus input
+	output [7:0] wb_dat_o;     // databus output
+	input        wb_we_i;      // write enable input
+	input        wb_stb_i;     // stobe/core select signal
+	input        wb_cyc_i;     // valid bus cycle input
+	output       wb_ack_o;     // bus cycle acknowledge output
+	output       wb_inta_o;    // interrupt request signal output
+
+	reg [7:0] wb_dat_o;
+	reg wb_ack_o;
+	reg wb_inta_o;
+
+	// I2C signals
+	// i2c clock line
+	input  scl_pad_i;       // SCL-line input
+	output scl_pad_o;       // SCL-line output (always 1'b0)
+	output scl_padoen_o;    // SCL-line output enable (active low)
+
+	// i2c data line
+	input  sda_pad_i;       // SDA-line input
+	output sda_pad_o;       // SDA-line output (always 1'b0)
+	output sda_padoen_o;    // SDA-line output enable (active low)
+
+
+	//
+	// variable declarations
+	//
+
+	// registers
+	reg  [15:0] prer; // clock prescale register
+	reg  [ 7:0] ctr;  // control register
+	reg  [ 7:0] txr;  // transmit register
+	wire [ 7:0] rxr;  // receive register
+	reg  [ 7:0] cr;   // command register
+	wire [ 7:0] sr;   // status register
+	reg  [ 6:0] sladr;// slave address register
+
+	// done signal: command completed, clear command register
+	wire done;
+	wire slave_done;
+	// core enable signal
+	wire core_en;
+	wire ien;
+	wire slave_en;
+	wire slave_dat_req;
+	wire slave_dat_avail;
+
+	// status register signals
+	wire irxack;
+	reg  rxack;       // received aknowledge from slave
+	reg  tip;         // transfer in progress
+	reg  irq_flag;    // interrupt pending flag
+	wire i2c_busy;    // bus busy (start signal detected)
+	wire i2c_al;      // i2c bus arbitration lost
+	reg  al;          // status register arbitration lost bit
+	reg  slave_mode;
+	//
+	// module body
+	//
+	wire  slave_act;
+	// generate internal reset
+	wire rst_i = arst_i ^ ARST_LVL;
+
+	// generate wishbone signals
+	wire wb_wacc = wb_we_i & wb_ack_o;
+
+	// generate acknowledge output signal ...
+	always @(posedge wb_clk_i)
+    // ... because timing is always honored.
+    wb_ack_o <=  wb_cyc_i & wb_stb_i & ~wb_ack_o;
+
+	// assign DAT_O
+	always @(posedge wb_clk_i)
+	begin
+	  case (wb_adr_i) // synopsys parallel_case
+	    3'b000: wb_dat_o <= prer[ 7:0];
+	    3'b001: wb_dat_o <= prer[15:8];
+	    3'b010: wb_dat_o <= ctr;
+	    3'b011: wb_dat_o <= rxr; // write is transmit register (txr)
+	    3'b100: wb_dat_o <= sr;  // write is command register (cr)
+	    3'b101: wb_dat_o <= txr; // Debug out of TXR
+	    3'b110: wb_dat_o <= cr;  // Debug out control reg
+	    3'b111: wb_dat_o <= {1'b0,sladr};   // slave address register
+	  endcase
+	end
+
+	// generate registers
+	always @(posedge wb_clk_i or negedge rst_i)
+	  if (!rst_i)
+	    begin
+	        prer <= 16'hffff;
+	        ctr  <=  8'h0;
+	        txr  <=  8'h0;
+	        sladr <=  DEFAULT_SLAVE_ADDR;
+	    end
+	  else if (wb_rst_i)
+	    begin
+	        prer <= 16'hffff;
+	        ctr  <=  8'h0;
+	        txr  <=  8'h0;
+	        sladr <=  DEFAULT_SLAVE_ADDR;
+	    end
+	  else
+	    if (wb_wacc)
+	      case (wb_adr_i) // synopsys parallel_case
+	         3'b000 : prer [ 7:0] <= wb_dat_i;
+	         3'b001 : prer [15:8] <= wb_dat_i;
+	         3'b010 : ctr         <= wb_dat_i;
+	         3'b011 : txr         <= wb_dat_i;
+	         3'b111 : sladr       <=  wb_dat_i[6:0];
+	         default: ;
+	      endcase
+
+	// generate command register (special case)
+	always @(posedge wb_clk_i or negedge rst_i)
+	  if (!rst_i)
+	    cr <= 8'h0;
+	  else if (wb_rst_i)
+	    cr <= 8'h0;
+	  else if (wb_wacc)
+	    begin
+	        if (core_en & (wb_adr_i == 3'b100) )
+	          cr <= wb_dat_i;
+	    end
+	  else
+	    begin
+	        cr[1] <=  1'b0;
+	        if (done | i2c_al)
+	          cr[7:4] <= 4'h0;           // clear command bits when done
+	                                        // or when aribitration lost
+	        cr[2] <=  1'b0;             // reserved bits
+	        cr[0]   <= 1'b0;             // clear IRQ_ACK bit
+	    end
+
+
+	// decode command register
+	wire sta  = cr[7];
+	wire sto  = cr[6];
+	wire rd   = cr[5];
+	wire wr   = cr[4];
+	wire ack  = cr[3];
+	wire sl_cont = cr[1];
+	wire iack = cr[0];
+
+	// decode control register
+	assign core_en = ctr[7];
+	assign ien = ctr[6];
+	assign slave_en = ctr[5];
+
+
+	// hookup byte controller block
+	i2c_master_byte_ctrl byte_controller (
+		.clk      ( wb_clk_i     ),
+		.my_addr  ( sladr        ),
+		.rst      ( wb_rst_i     ),
+		.nReset   ( rst_i        ),
+		.ena      ( core_en      ),
+		.clk_cnt  ( prer         ),
+		.start    ( sta          ),
+		.stop     ( sto          ),
+		.read     ( rd           ),
+		.write    ( wr           ),
+		.ack_in   ( ack          ),
+		.din      ( txr          ),
+		.cmd_ack  ( done         ),
+		.ack_out  ( irxack       ),
+		.dout     ( rxr          ),
+		.i2c_busy ( i2c_busy     ),
+		.i2c_al   ( i2c_al       ),
+		.scl_i    ( scl_pad_i    ),
+		.scl_o    ( scl_pad_o    ),
+		.scl_oen  ( scl_padoen_o ),
+		.sda_i    ( sda_pad_i    ),
+		.sda_o    ( sda_pad_o    ),
+		.sda_oen  ( sda_padoen_o ),
+		.sl_cont  ( sl_cont       ),
+		.slave_en ( slave_en      ),
+		.slave_dat_req (slave_dat_req),
+		.slave_dat_avail (slave_dat_avail),
+		.slave_act (slave_act),
+		.slave_cmd_ack (slave_done)
+	);
+
+	// status register block + interrupt request signal
+	always @(posedge wb_clk_i or negedge rst_i)
+	  if (!rst_i)
+	    begin
+	        al       <= 1'b0;
+	        rxack    <= 1'b0;
+	        tip      <= 1'b0;
+	        irq_flag <= 1'b0;
+	        slave_mode <=  1'b0;
+	    end
+	  else if (wb_rst_i)
+	    begin
+	        al       <= 1'b0;
+	        rxack    <= 1'b0;
+	        tip      <= 1'b0;
+	        irq_flag <= 1'b0;
+	        slave_mode <=  1'b0;
+	    end
+	  else
+	    begin
+	        al       <= i2c_al | (al & ~sta);
+	        rxack    <= irxack;
+	        tip      <= (rd | wr);
+	        // interrupt request flag is always generated
+	        irq_flag <=  (done | slave_done| i2c_al | slave_dat_req |
+	        		      slave_dat_avail | irq_flag) & ~iack;
+	        if (done)
+	          slave_mode <=  slave_act;
+
+	    end
+
+	// generate interrupt request signals
+	always @(posedge wb_clk_i or negedge rst_i)
+	  if (!rst_i)
+	    wb_inta_o <= 1'b0;
+	  else if (wb_rst_i)
+	    wb_inta_o <= 1'b0;
+	  else
+        // interrupt signal is only generated when IEN (interrupt enable bit
+        // is set)
+        wb_inta_o <=  irq_flag && ien;
+
+	// assign status register bits
+	assign sr[7]   = rxack;
+	assign sr[6]   = i2c_busy;
+	assign sr[5]   = al;
+	assign sr[4]   = slave_mode; // reserved
+	assign sr[3]   = slave_dat_avail;
+	assign sr[2]   = slave_dat_req;
+	assign sr[1]   = tip;
+	assign sr[0]   = irq_flag;
 
 endmodule
+
 `default_nettype wire
+
